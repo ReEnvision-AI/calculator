265b95e78a1731ff1971ea8981eddaa11c51c08a
Bradley Jones <pgawestjones@gmail.com>
2024-11-12 16:32:51 -0800

    Udated css and classnames

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Calculator/>
+        <div className="absolute inset-0 pb-16 pt-safe overflow-hidden"
+            style={{
+                background: 'linear-gradient(to right bottom, #2D3436, #000428, #004E92, #000428, #2D3436)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed'
+            }}>
+                <div className={`flex flex-col h-full bg-gray-800 rounded-lg shadow-xl border border-gray-700 ring-2 ring-blue-500/50`}>
+                    <div className="flex-1 p-4 overflow-auto scroll-rubber-band">
+                        <Calculator/>
+                    </div>
+                </div>
+        </div>
     </StrictMode>
 )
